b7a69dc046be6ab1c88b60022c5ea4271edce9b6
Minoru-Abe <tomominoru.abe@gmail.com>
2024-09-05 21:15:04 +0900

    Up to step6

--- src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRUnner.java
+++ src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRUnner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.in28minutes.springboot.learn_jpa_and_hibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRUnner implements CommandLineRunner{
 	
@@ -12,7 +14,11 @@ public class CourseJdbcCommandLineRUnner implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert();
+		repository.insert(new Course(1, "Learn AWS Now!", "in28minutes"));
+		repository.insert(new Course(2, "Learn Azure Now!", "in28minutes"));
+		repository.insert(new Course(3, "Learn DeepLearning Now!", "in28minutes"));
+		repository.deleteById(2);
+		
 		
 	}
 
